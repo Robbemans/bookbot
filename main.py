7998234f2e8567fd8e2817a1a63f115e8397f783
Robbemans <Robertblaak@outlook.com>
2025-11-14 16:28:51 +0100

    edit stats.py, edit main.py

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from stats import count_words
 from stats import count_characters
 from stats import sort_dict
-from stats import filepath
+import sys
+
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+filepath = sys.argv[1]
 
 def main():
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {filepath}")
     print("----------- Word Count ----------")
-    print(f"Found {count_words()} total words")
+    print(f"Found {count_words(filepath)} total words")
     print("--------- Character Count -------")
-    for dict in sort_dict():
+    for dict in sort_dict(filepath):
         if dict["char"].isalpha() is True:
             print(f"{dict["char"]}: {dict["num"]}")
         else:
--- stats.py
+++ stats.py
@@ -1,16 +1,15 @@
-filepath = "books/frankenstein.txt"
 
 def get_book_text(filepath):
     with open(filepath) as f:
        text = f.read()
     return text
 
-def count_words():
+def count_words(filepath):
     text = get_book_text(filepath)
     number_of_words = len(text.split())
     return number_of_words
 
-def count_characters():
+def count_characters(filepath):
     text = get_book_text(filepath).lower()
     characters = {}
 
@@ -24,10 +23,10 @@ def count_characters():
 def sort_on(characters):
     return characters["num"]
 
-def sort_dict():
-    characters = count_characters()
+def sort_dict(filepath):
+    characters = count_characters(filepath)
     info_list = []
-    for character in count_characters():
+    for character in characters:
         info = {
             "char": character,
             "num": characters[character],
